8777b6c65d8eee1c3a31febe114c06d4f7a6b903
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2020-05-14 23:07:19 -0400

    react navigation integrated

---
+++ app/App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./containers/home/Home";
+import SettingsScreen from "./containers/settings/Settings";
+
+const Tab = createBottomTabNavigator();
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
